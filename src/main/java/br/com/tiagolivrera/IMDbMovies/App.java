c34b7d9761f0645e6b2e8ad2567034055aac41c8
Thyago de Oliveira Freitas <thyago.olifreitas@gmail.com>
2023-07-13 12:57:47 -0300

    Desafio dia 1

--- src/main/java/br/com/tiagolivrera/IMDbMovies/App.java
+++ src/main/java/br/com/tiagolivrera/IMDbMovies/App.java
@@ -1,13 +1,16 @@
 package br.com.tiagolivrera.IMDbMovies;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class App {
+	public static void main(String[] args) throws IOException, InterruptedException {
+		ConsumerRequest cr = new ConsumerRequest();
+		File file = new File("out.json");
+		PrintStream printStream = new PrintStream(new FileOutputStream(file));
+		System.setOut(printStream);
+		System.out.println(cr.getUpcomingMovies());
+	}
 }
---
+++ src/main/java/br/com/tiagolivrera/IMDbMovies/ConsumerRequest.java
@@ -0,0 +1,23 @@
+package br.com.tiagolivrera.IMDbMovies;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsumerRequest {
+
+	private static final String API_KEY = "";
+	private static final String API_URL_UPCOMING = "https://moviesdatabase.p.rapidapi.com/titles/x/upcoming";
+	private static final String API_URL = "moviesdatabase.p.rapidapi.com";
+
+	public String getUpcomingMovies() throws IOException, InterruptedException {
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create(API_URL_UPCOMING))
+				.header("X-RapidAPI-Key", API_KEY).header("X-RapidAPI-Host", API_URL)
+				.method("GET", HttpRequest.BodyPublishers.noBody()).build();
+		HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+		return response.body();
+	}
+
+}
